1ec02b7c3426672b968060e866041ad8728d50ad
Alexander Frolov <big_air2409@mail.ru>
2017-12-07 13:19:29 +1300

    Moved Book Component into its own file

---
+++ src/Book.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+
+class Book extends Component {
+
+  render() {
+
+    const {bookInstance} = this.props
+
+    var title,
+        thumbnail,
+        authors = ""
+
+    if(bookInstance){
+      title = bookInstance.title
+      thumbnail = bookInstance.imageLinks.smallThumbnail
+      bookInstance.authors.forEach((value, index) => {
+        if(index > 0){
+          authors += (", " + value)
+        } else {
+          authors += value
+        }
+      })
+    }
+
+    return(
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{
+                                            width: 128,
+                                            height: 188,
+                                            backgroundImage: `url(${thumbnail})` }}></div>
+          <div className="book-shelf-changer">
+            <select id={bookInstance.id} onChange={this.props.onShelfChange}>
+              <option value="none" disabled>Move to...</option>
+              <option value="none">None</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors}</div>
+      </div>
+    )//return()
+  }//render()
+}//class Book
+
+export default Book
